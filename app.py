c5b9d1d78a603550c962ce1cd3c0dbe2e3c5d6f9
Francisca Moore <fraanmoore@gmail.com>
2023-04-23 17:36:30 -0400

    do tables and routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from models import db, User
+from models import db, User, Character, Favorite
 
 # @app.route("/", methods=["GET", "POST", "PUT", "DELETE"]) #decorador, escucha la ruta y metodo declarada para entrar a ese recurso
 # def home():
@@ -12,7 +12,7 @@ db.init_app(app)
 
 @app.route("/")
 def home():
-    return "Hello SQLAlchemy y Flask"
+    return "Hello backend"
 
 @app.route("/users", methods=["POST"])
 def create_user():
@@ -34,7 +34,6 @@ def get_users():
         result.append(user.serialize())
     return jsonify(result)
 
-
 @app.route("/users/<int:id>", methods=["PUT", "DELETE"])
 def update_user(id):
     user = User.query.get(id)
@@ -53,6 +52,97 @@ def update_user(id):
     
     return jsonify("Usuario no encontrado"), 418
 
+@app.route("/characters", methods=["POST"])
+def create_character():
+    character = Character()
+    character.id = request.json.get("id")
+    character.name = request.json.get("name")
+    character.actor = request.json.get("actor")
+    character.house = request.json.get("house")
+    character.date_of_birth = request.json.get("date_of_birth")
+    character.patronus = request.json.get("patronus")
+    character.half_blood = request.json.get("half_blood")
+
+    db.session.add(character)
+    db.session.commit()
+
+    return "personaje guardado"
+
+@app.route("/characters/list", methods=["GET"])
+def get_character():
+    character = Character.query.all()
+    result = []
+    for character in character:
+        result.append(character.serialize())
+    return jsonify(result)
+
+@app.route("/characters/<int:id>", methods=["PUT", "DELETE"])
+def update_character(id):
+    character = Character.query.get(id)
+    if character is not None:
+        if request.method == "DELETE":
+            db.session.delete(character)
+            db.session.commit()
+
+            return jsonify("Eliminado"), 204
+        else:
+            character.name= request.json.get("name")
+            
+            db.session.commit()
+            
+            return jsonify("personaje actualizado"), 200
+    
+    return jsonify("personaje no encontrado"), 418
+
+@app.route("/favorites", methods=["POST"])
+def create_favorite():
+    favorite = Favorite()
+    favorite.username = request.json.get("username")
+    favorite.name = request.json.get("name")
+    favorite.character_id = request.json.get("character_id")
+   
+  
+    db.session.add(favorite)
+    db.session.commit()
+    
+    return jsonify("Favorito guardado"), 201
+
+@app.route("/favorites/list", methods=["GET"])
+def get_favorites():
+    favorites = Favorite.query.all()
+    result = []
+    for favorite in favorites:
+        result.append(favorite.serialize())
+    return jsonify(result)
+
+@app.route("/favorite/<int:id>", methods=["PUT", "DELETE"])
+def update_favorite(id):
+    favorite = Favorite.query.get(id)
+    if favorite is not None:
+        if request.method == "DELETE":
+            db.session.delete(favorite)
+            db.session.commit()
+            
+            return jsonify("Eliminado"), 204
+        else:
+            favorite.character_id = request.json.get("character_id")
+            favorite.name = request.json.get("name")
+            favorite.id_user = request.json.get("id_user")
+       
+            db.session.commit()
+        
+            return jsonify("Favorite actualizado"), 200
+    
+    return jsonify("Favorite no encontrado"), 404
+
+
+@app.route("/favorite/user/<int:user_id>", methods=["GET"])
+def get_favorite_user(user_id):
+    favorites = Favorite.query.filter_by(id_user=user_id).all()
+    result = []
+    for favorite in favorites:
+        result.append(favorite.serialize())
+    return jsonify(result)
 
 
 with app.app_context():
--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.db.Column(db.String(20), nullable=False)
     age = db.Column(db.Integer)
 
     def serialize(self):
@@ -15,4 +15,43 @@ class User(db.Model):
             "username": self.username,
             "id": self.id,
             "age": self.age
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Integer, db.ForeignKey('user.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    name = db.Column(db.String(50), nullable=False)
+  
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name" : self.name,
+            "username" : self.username,
+            "character_id" : self.character_id
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    actor = db.Column(db.String(50), nullable=False)
+    house = db.Column(db.String(50), nullable=False)
+    date_of_birth = db.Column(db.String(50))
+    patronus = db.Column(db.String(50))
+    half_blood = db.Column(db.String(50))
+    favorite_username = db.Column(db.Integer, db.ForeignKey('favorite.username'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "actor": self.actor,
+            "house": self.house,
+            "date_of_birth": self.date_of_birth,
+            "patronus": self.patronus,
+            "half_blood": self.half_blood,
+            "favorite_username": self.favorite_username,
         }
